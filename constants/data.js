c967f45e2876089a4a707d2f0d2e89be2a316fb7
Kenneth Chan <slayerkenneth@gmail.com>
2023-02-27 17:29:49 +0800

    feat: new layout wrt. template

---
+++ constants/data.js
@@ -0,0 +1,83 @@
+const userData = {
+  githubUsername: "slayerkenneth",
+  name: "Kenneth Chan Wing Lok",
+  designation: "Game Developer & Programmer & Multimedia Engineer & Drummer",
+  avatarUrl: "/mePicTemp.png",
+  email: "kennethchanwl.dev@gmail.com",
+  phone: "+852 51085539",
+  address: "Hong Kong",
+  projects: [
+    {
+      title: "Death Match Olympus",
+      link: "https://youtu.be/nbxN4XWsJi0",
+      imgUrl: "/DMOlympus.png",
+    },
+    {
+      title: "Intelligent-Safety-System-Assisting-Cyclist",
+      link: "https://github.com/slayerkenneth/Intelligent-Safety-System-Assisting-Cyclist",
+      imgUrl: "/ISSAC_System.jpg",
+    },
+    {
+      title: "Portfolio",
+      link: "https://slayerkenneth.github.io/",
+      imgUrl: "/portfolio.png", // to be changed
+    },
+    {
+      title: "Human Computer Interaction",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+  ],
+  about: {
+    title:
+      "Hi~ I am a game developer and multimedia engineer who believes that creativity is the most precious and irreplaceable thing from human being",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "HKUST Final Year Project - BoAR, AR mobile Game",
+    currentProjectUrl: "https://github.com/slayerkenneth/FYP_BoAR_PSAN2_38",
+  },
+  experience: [
+    {
+      title: "Internship & Part-time - Game Developer",
+      company: "Bubstal Ltd.",
+      year: "2022",
+      companyLink: "https://www.bubstal.com/",
+      desc: "",
+    },
+    {
+      title: "Internship - Software Engineer",
+      company: "Bon Bon Robotics Ltd.",
+      year: "2019",
+      companyLink: "",
+      desc: "",
+    },
+    {
+      title: "Student Helper - Mobile App Developer",
+      company: "Path Advisor - ITSC, HKUST",
+      year: "2020",
+      companyLink: "",
+      desc: "",
+    },
+    {
+      title: "Final Year Student",
+      company: "CPEG, HKUST, Hong Kong",
+      year: "2023",
+      companyLink: "https://hkust.edu.hk/",
+      desc: "Major in Computer Engineering",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1-ex6v5gHXXlew9-gYxGozRtTmYkcnTBg/view?usp=share_link",
+  socialLinks: {
+    instagram: "https://www.instagram.com/xx_skc/",
+    twitter: "https://twitter.com/SlayerLok",
+    linkedin: "https://www.linkedin.com/in/kenneth-chan-gamedev/",
+    github: "https://github.com/slayerkenneth",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
